334491c1cdec86dd55f4690b8b392bf017efbe14
HeraclitoDeEfeso <eloscurodeefeso@gmail.com>
2020-04-18 20:50:02 -0300

    Reloj interno. Apago otras unidades analogicas

--- TP1/ejercicio2.c
+++ TP1/ejercicio2.c
@@ -1,6 +1,6 @@
 #define _XTAL_FREQ 4000000
 
-#pragma config FOSC = EXTRCIO
+#pragma config FOSC = INTRCIO
 #pragma config WDTE = OFF 
 #pragma config PWRTE = OFF 
 #pragma config MCLRE = OFF 
@@ -12,10 +12,12 @@
 
 void main()
 {
-    CMCON = 0b00000111;     // Disable comparators
-    ANSEL = 0;              // Disable analog signal
-    TRISIO = 0b00111110;    // PIN0 output 
-    GPIO = 0;               // All output to low
+    CMCON   = 0x07; // Disable comparators
+    ADCON0  = 0;    // Disable analog converter
+    ANSEL   = 0;    // Disable analog signal
+    VRCON   = 0;    // Disable voltaje reference
+    TRISIO  = 0x3E; // PIN0 output 
+    GPIO    = 0;    // All output to low
     
     while (1)
     {
