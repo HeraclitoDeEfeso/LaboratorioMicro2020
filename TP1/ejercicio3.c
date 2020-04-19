4c1d4cb25d943553295573bd00d389202682f29f
HeraclitoDeEfeso <eloscurodeefeso@gmail.com>
2020-04-19 18:40:53 -0300

    Elimino test y repetición de main

--- TP1/ejercicio3.c
+++ TP1/ejercicio3.c
@@ -20,22 +20,8 @@ void init()
 {
     CMCON = 0b00000111;
     ANSEL = 0;
-    TRISIO = 0b11111110; //bit en 1 es input, en 0 es output. Seteo GP0 como OUTPUT y los demás como INPUT.
+    TRISIO = 0b11111110; //bit en 1 es input, en 0 es output. Seteo GP0 como OUTPUT y los demï¿½s como INPUT.
     GPIO = 0; //clear GPbits.
-}
-
-void test()
-{
-    GP1 = 0;
-    __delay_ms(2000);//apagado 2 segundos
-    GP1 = 1;
-    __delay_ms(1);
-    GP1 = 0;
-    __delay_ms(5000);//parpadeando 5 segundos
-    GP1 = 1;
-    __delay_ms(1);
-    GP1 = 0;
-    __delay_ms(5000);//apagado 5 segundos
 }
 
 int wait(){ //devuelve 1 si se debe cambiar el estado
@@ -55,38 +41,6 @@ int wait(){ //devuelve 1 si se debe cambiar el estado
     return 0;
 }
 
-void alternoled(){ //cambio el valor de la salida(LED))
-    if(GP0 == 1){
-        GP0 = 0 ;
-    }else GP0 = 1 ;
-}
-
-void main(){
-    init();
-    GP0 = 0;
-    GP1 = 0;
-    int estado = 0;
-    while(1){
-        switch(estado){
-            case 0://inicial(llego aca con el led apagado) 
-                if(wait() == 1){
-                   estado = 1; 
-                }
-            break;
-            case 1://encendido(llego aca con el led apagado)
-                alternoled();
-                if(wait() == 1){
-                   estado = 2;
-                   break;
-                }              
-            break; 
-            case 2://apagado(llego aca con el parpadeo activo)
-                GP0 = 0;
-                estado = 0;
-                break;
-        }           
-    }
-}
 void alternoled(){ //cambio el valor de la salida(LED))
     if(GP0 == 1){
         GP0 = 0 ;
