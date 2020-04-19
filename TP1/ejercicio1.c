e4c7dae179749fa21391f07ca5d1068590d73609
HeraclitoDeEfeso <eloscurodeefeso@gmail.com>
2020-04-19 19:36:20 -0300

    Reemplazo Ejercicio 1 para explicarlo

--- TP1/ejercicio1.c
+++ TP1/ejercicio1.c
@@ -1,41 +1,35 @@
 #define _XTAL_FREQ 4000000
 
-#pragma config FOSC = EXTRCIO
-#pragma config WDTE = OFF // Watchdog Timer Enable bit (WDT disabled)
-#pragma config PWRTE = OFF // Power-Up Timer Enable bit (PWRT disabled)
-#pragma config MCLRE = OFF // GP3/MCLR pin function select (GP3/MCLR pin function is digital I/O, MCLR internally tied to VDD)
-#pragma config BOREN = OFF // Brown-out Detect Enable bit (BOD disabled)
-#pragma config CP = OFF // Code Protection bit (Program Memory code protection is disabled)
-#pragma config CPD = OFF // Data Code Protection bit (Data memory code protection is disabled)
+#pragma config FOSC = INTRCIO
+#pragma config WDTE = OFF 
+#pragma config PWRTE = OFF 
+#pragma config MCLRE = OFF 
+#pragma config BOREN = OFF 
+#pragma config CP = OFF 
+#pragma config CPD = OFF 
 
 #include <xc.h>
 
-void init() 
-{
-    CMCON = 0b00000111;
-    ANSEL = 0;
-    TRISIO = 0b11111110; //bit en 1 es input, en 0 es output. Seteo GP0 como OUTPUT y los demás como INPUT.
-    GPIO = 0; //clear GPbits.
-}
-
-void test()
-{
-    GP0 = 1;
-    __delay_ms(500);
-    GP0 = 0;
-    __delay_ms(500);
-}
-
 void main()
 {
-    init();
-    while(1)
+    CMCON   = 0x07; // Disable comparators
+    ADCON0  = 0;    // Disable analog converter
+    ANSEL   = 0;    // Disable analog signal
+    VRCON   = 0;    // Disable voltaje reference
+    TRISIO  = 0x3E; // PIN0 output 
+    GPIO    = 0;    // All output to low
+    
+    while (1)
     {
-	GP0 = 0;
-	if (GP1 == 1)
-	{
-	    GP0 = ~GP0;
-	    __delay_ms(100); //Por alguna razón hay que darle un delay, no sé si es por el emulador, o por alguna especificación que no conozco del manejador de eventos del pic.
-	}
+	    if (GP1 == 1)
+	    {
+	        __delay_ms(10);
+            if (GP1 == 1)
+            {
+                GP0 = 1;
+            }
+            while (GP1 == 1);
+            GP0 = 0;
+	    }
     }
 }
