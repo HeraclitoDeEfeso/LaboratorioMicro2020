6d63bc5da8f9d5d9289838e00b4da1ad38c24563
HeraclitoDeEfeso <eloscurodeefeso@gmail.com>
2020-04-19 19:03:11 -0300

    Arreglo Reloj

--- TP1/ejercicio3.c
+++ TP1/ejercicio3.c
@@ -7,41 +7,24 @@
 
 #define _XTAL_FREQ 4000000
 
-#pragma config FOSC = EXTRCIO
-#pragma config WDTE = OFF // Watchdog Timer Enable bit (WDT disabled)
-#pragma config PWRTE = OFF // Power-Up Timer Enable bit (PWRT disabled)
-#pragma config MCLRE = OFF // GP3/MCLR pin function select (GP3/MCLR pin function is digital I/O, MCLR internally tied to VDD)
-#pragma config BOREN = OFF // Brown-out Detect Enable bit (BOD disabled)
-#pragma config CP = OFF // Code Protection bit (Program Memory code protection is disabled)
-#pragma config CPD = OFF // Data Code Protection bit (Data memory code protection is disabled)
+#pragma config FOSC = INTRCIO
+#pragma config WDTE = OFF 
+#pragma config PWRTE = OFF 
+#pragma config MCLRE = OFF 
+#pragma config BOREN = OFF 
+#pragma config CP = OFF 
+#pragma config CPD = OFF 
+
 #include <xc.h>
 
-void init() 
-{
-    CMCON = 0b00000111;
-    ANSEL = 0;
-    TRISIO = 0b11111110; //bit en 1 es input, en 0 es output. Seteo GP0 como OUTPUT y los demás como INPUT.
-    GPIO = 0; //clear GPbits.
-}
-
-void test()
-{
-    GP1 = 0;
-    __delay_ms(2000);//apagado 2 segundos
-    GP1 = 1;
-    __delay_ms(1);
-    GP1 = 0;
-    __delay_ms(5000);//parpadeando 5 segundos
-    GP1 = 1;
-    __delay_ms(1);
-    GP1 = 0;
-    __delay_ms(5000);//apagado 5 segundos
-}
-
-int wait(){ //devuelve 1 si se debe cambiar el estado
+// devuelve 1 si se debe cambiar el estado
+int wait(){ 
     int entradaanterior = GP1;
     int contador = 0;
-    while(contador < 1000){ //el ciclo tardara un segundo salvo que se apriete y se suelte el pulsador
+
+    // el ciclo tardara un segundo 
+    // salvo que se apriete y se suelte el pulsador
+    while(contador < 1000){ 
         __delay_ms(1);
         if(entradaanterior == 1 && GP1 == 0){//solte el boton
             return 1;
@@ -52,17 +35,13 @@ int wait(){ //devuelve 1 si se debe cambiar el estado
     return 0;
 }
 
-void alternoled(){ //cambio el valor de la salida(LED))
-    if(GP0 == 1){
-        GP0 = 0 ;
-    }else GP0 = 1 ;
-}
-
 void main(){
-    init();
-    GP0 = 0;
-    GP1 = 0;
+    CMCON = 0b00000111;
+    ANSEL = 0;
+    TRISIO = 0b11111110; // Seteo GP0 como OUTPUT y los demás como INPUT.
+    GPIO = 0; //clear GPbits.
     int estado = 0;
+
     while(1){
         switch(estado){
             case 0://inicial(llego aca con el led apagado) 
@@ -71,7 +50,7 @@ void main(){
                 }
             break;
             case 1://encendido(llego aca con el led apagado)
-                alternoled();
+                GP0 = ~GP0;
                 if(wait() == 1){
                    estado = 2;
                    break;
