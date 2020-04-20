dc8fd2165ff419c3658cd5cf37c6fbcbb66d1038
HeraclitoDeEfeso <eloscurodeefeso@gmail.com>
2020-04-20 17:23:01 -0300

    Propuesta de Ejercicio 3 con timer

---
+++ TP1/ejercicio3bis.c
@@ -0,0 +1,55 @@
+#define _XTAL_FREQ 4000000
+
+#pragma config FOSC = INTRCIO
+#pragma config WDTE = OFF 
+#pragma config PWRTE = OFF 
+#pragma config MCLRE = ON 
+#pragma config BOREN = OFF 
+#pragma config CP = OFF 
+#pragma config CPD = OFF 
+
+#include <xc.h>
+
+unsigned char blink = 0;// Blinking state
+unsigned int count = 0; // Blinking time counter
+
+void main()
+{
+    CMCON   = 0x07;     // Disable comparators
+    ADCON0  = 0;        // Disable analog converter
+    ANSEL   = 0;        // Disable analog signal
+    VRCON   = 0;        // Disable voltage reference
+    TRISIO  = 0x3E;     // PIN0 output 
+    GPIO    = 0;        // All output to low
+    OPTION_REG = 0xC7;  // Set Timer0 prescaler to 256 clocks
+    T0IE = 1;           // Enable Timer0 interrupt
+    GIE = 1;            // Enable global interrupt
+
+    while (1)
+    {
+	    if (GP1 == 1)
+	    {
+	        __delay_ms(10);
+            if (GP1 == 1)
+            {
+                GP0 = ~blink;
+                blink = ~blink;
+            }
+            while (GP1 == 1);
+	    }
+    }
+}
+
+void interrupt ISR(void)
+{
+    if(T0IF)
+    {
+        count++;
+        if (count >= 10 && blink)
+        {
+            GP0 = ~GP0;
+            count = 0;
+        }
+        T0IF = 0;
+    }
+}
