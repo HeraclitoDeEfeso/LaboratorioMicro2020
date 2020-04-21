25215e3a78e6656969c9e3baa3736a28178be768
HeraclitoDeEfeso <eloscurodeefeso@gmail.com>
2020-04-21 11:51:18 -0300

    Minimizo inicializacion

--- TP1/ejercicio2.c
+++ TP1/ejercicio2.c
@@ -1,21 +1,19 @@
 #define _XTAL_FREQ 4000000
 
-#pragma config FOSC = INTRCIO
-#pragma config WDTE = OFF 
-#pragma config PWRTE = OFF 
-#pragma config MCLRE = OFF 
-#pragma config BOREN = OFF 
-#pragma config CP = OFF 
-#pragma config CPD = OFF 
+#pragma config FOSC     = INTRCIO
+#pragma config WDTE     = OFF 
+#pragma config PWRTE    = OFF 
+#pragma config MCLRE    = OFF 
+#pragma config BOREN    = OFF 
+#pragma config CP       = OFF 
+#pragma config CPD      = OFF 
 
 #include <xc.h>
 
 void main()
 {
     CMCON   = 0x07; // Disable comparators
-    ADCON0  = 0;    // Disable analog converter
     ANSEL   = 0;    // Disable analog signal
-    VRCON   = 0;    // Disable voltage reference
     TRISIO  = 0x3E; // PIN0 output 
     GPIO    = 0;    // All output to low
     
