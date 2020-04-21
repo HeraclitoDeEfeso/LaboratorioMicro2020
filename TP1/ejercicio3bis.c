50fe2c03bbdbd8bab076f7eb67a73ef31317ddc5
HeraclitoDeEfeso <eloscurodeefeso@gmail.com>
2020-04-21 11:45:31 -0300

    Uso timer1 de 16 bits. Reduzco código

--- TP1/ejercicio3bis.c
+++ TP1/ejercicio3bis.c
@@ -1,29 +1,24 @@
 #define _XTAL_FREQ 4000000
 
-#pragma config FOSC = INTRCIO
-#pragma config WDTE = OFF 
-#pragma config PWRTE = OFF 
-#pragma config MCLRE = OFF 
-#pragma config BOREN = OFF 
-#pragma config CP = OFF 
-#pragma config CPD = OFF 
+#pragma config FOSC     = INTRCIO
+#pragma config WDTE     = OFF 
+#pragma config PWRTE    = OFF 
+#pragma config MCLRE    = OFF 
+#pragma config BOREN    = OFF 
+#pragma config CP       = OFF 
+#pragma config CPD      = OFF 
 
 #include <xc.h>
 
-unsigned char blink = 0;// Blinking state
-unsigned int count = 0; // Blinking time counter
-
 void main()
 {
-    CMCON   = 0x07;     // Disable comparators
-    ADCON0  = 0;        // Disable analog converter
-    ANSEL   = 0;        // Disable analog signal
-    VRCON   = 0;        // Disable voltage reference
-    TRISIO  = 0x3E;     // PIN0 output 
-    GPIO    = 0;        // All output to low
-    OPTION_REG = 0xC7;  // Set Timer0 prescaler to 256 clocks
-    T0IE = 1;           // Enable Timer0 interrupt
-    GIE = 1;            // Enable global interrupt
+    CMCON       = 0x07;     // Disable comparators
+    ANSEL       = 0;        // Disable analog signal
+    TRISIO      = 0x3E;     // PIN0 output 
+    GPIO        = 0;        // All output to low
+    T1CON       = 0x31;     // Set Timer1 prescaler to 8 clocks
+    PEIE        = 1;        // Enable peripherical interrupt
+    GIE         = 1;        // Enable global interrupt
 
     while (1)
     {
@@ -32,9 +27,8 @@ void main()
 	        __delay_ms(10);
             if (GP1 == 1)
             {
-                GP0 = ~blink;
-                blink = ~blink;
-                count = 0;
+                GP0 = ~T1IE;
+                T1IE = ~T1IE;
             }
             while (GP1 == 1);
 	    }
@@ -43,14 +37,9 @@ void main()
 
 void interrupt ISR(void)
 {
-    if(T0IF)
+    if(T1IF)
     {
-        count++;
-        if (count >= 10 && blink)
-        {
-            GP0 = ~GP0;
-            count = 0;
-        }
-        T0IF = 0;
+        GP0 = ~GP0;
+        T1IF = 0;
     }
 }
