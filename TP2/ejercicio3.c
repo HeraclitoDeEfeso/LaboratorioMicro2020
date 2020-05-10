0597379853a50a849076f834adbfb1742be8aae1
Matias Monti <33459048+MatiasMonti@users.noreply.github.com>
2020-05-09 23:00:11 -0300

    Ejercicio practico 3
    archivo C y Hex los cuales son los del tp1 y nuevo esquematico segun lo solicitado en el tp2

---
+++ TP2/ejercicio3.c
@@ -0,0 +1,33 @@
+#define _XTAL_FREQ 4000000
+
+#pragma config FOSC     = INTRCIO
+#pragma config WDTE     = OFF 
+#pragma config PWRTE    = OFF 
+#pragma config MCLRE    = OFF 
+#pragma config BOREN    = OFF 
+#pragma config CP       = OFF 
+#pragma config CPD      = OFF 
+
+#include <xc.h>
+
+void main()
+{
+    CMCON   = 0x07; // Disable comparators
+    ANSEL   = 0;    // Disable analog signal
+    TRISIO  = 0x3E; // PIN0 output 
+    GPIO    = 0;    // All output to low
+    
+    while (1)
+    {
+	    if (GP1 == 1)
+	    {
+	        __delay_ms(10);
+            if (GP1 == 1)
+            {
+                GP0 = 1;
+            }
+            while (GP1 == 1);
+            GP0 = 0;
+	    }
+    }
+}
